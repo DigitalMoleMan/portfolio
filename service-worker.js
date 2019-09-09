c7b8d57de02c65eca546d362a6f8306fdc91025f
Wille Pilquist <wille.pilquist@learnet.se>
2019-09-09 11:43:47 +0200

    styling

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const filesToCache = [
+    '/',
+    'style.css',
+    'index.html',
+    'service-worker.js'
+  ];
+const staticCacheName = 'wp-portfolio-cache';
+
+self.addEventListener('install', event => {
+    console.log('Attempting to install service worker and cache static assets');
+    event.waitUntil(
+      caches.open(staticCacheName)
+      .then(cache => {
+        return cache.addAll(filesToCache);
+      })
+    );
+  });
+
+self.addEventListener('fetch', event => {
+    console.log('Fetch event for ', event.request.url);
+    event.respondWith(
+      caches.match(event.request)
+      .then(response => {
+        if (response) {
+          console.log('Found ', event.request.url, ' in cache');
+          return response;
+        }
+        console.log('Network request for ', event.request.url);
+        return fetch(event.request)
+  
+        .then(response => {
+            return caches.open(staticCacheName).then(cache => {
+              cache.put(event.request.url, response.clone());
+              return response;
+            });
+          });
+  
+      }).catch(error => {
+          console.log(error);
+      })
+    );
+  });
