1999659fe234c7552ed6d1613ddcc43678c42d64
Wille Pilquist <wille.pilquist@learnet.se>
2019-09-10 11:43:43 +0200

    added item to filesToCache list

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,25 @@
 const filesToCache = [
-    'style.css',
-    'index.html',
-    'service-worker.js'
-  ];
+  './',
+  'style.css',
+  'index.html',
+  'service-worker.js'
+];
 const staticCacheName = 'wp-portfolio-cache';
 
 self.addEventListener('install', event => {
-    console.log('Attempting to install service worker and cache static assets');
-    event.waitUntil(
-      caches.open(staticCacheName)
+  console.log('Attempting to install service worker and cache static assets');
+  event.waitUntil(
+    caches.open(staticCacheName)
       .then(cache => {
         return cache.addAll(filesToCache);
       })
-    );
-  });
+  );
+});
 
 self.addEventListener('fetch', event => {
-    console.log('Fetch event for ', event.request.url);
-    event.respondWith(
-      caches.match(event.request)
+  console.log('Fetch event for ', event.request.url);
+  event.respondWith(
+    caches.match(event.request)
       .then(response => {
         if (response) {
           console.log('Found ', event.request.url, ' in cache');
@@ -26,16 +27,16 @@ self.addEventListener('fetch', event => {
         }
         console.log('Network request for ', event.request.url);
         return fetch(event.request)
-  
-        .then(response => {
+
+          .then(response => {
             return caches.open(staticCacheName).then(cache => {
               cache.put(event.request.url, response.clone());
               return response;
             });
           });
-  
+
       }).catch(error => {
-          console.log(error);
+        console.log(error);
       })
-    );
-  });
+  );
+});
